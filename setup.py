3786be18968146048bffb642d5b5b4703c530070
Samuel Byrne Klein <samuel.byrne.klein@cern.ch>
2021-12-01 15:17:08 +0100

    Removing description.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 setup(
     name="surVAE",
     version='0.01',
     description="Experiments for studying distribution matching.",
-    long_description=long_description,
+    # long_description=long_description,
     long_description_content_type='text/markdown',
     # url="https://github.com/sambklein/distribution_matching",
     author="Sam Klein",
