705200211d57ce45c1cef92bcb29e866373e129f
Samuel Byrne Klein <samuel.byrne.klein@cern.ch>
2021-12-03 15:44:31 +0100

    Final push before checking if repo really works.

--- run_setup.sh
+++ run_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -O surVAE/data/downloads/data.tar.gz "https://zenodo.org/record/1161203/files/data.tar.gz?download=1"
 tar -xvf surVAE/data/downloads/data.tar.gz
 mv surVAE/data/downloads/data/* surVAE/data/downloads/
